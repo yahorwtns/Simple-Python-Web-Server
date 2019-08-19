c4c284db92dee66dabfcb232a685c3358e486b08
Yahor <k.minchenok@gmail.com>
2019-08-19 16:22:37 +0300

    Update serv.py
    change 123

--- serv.py
+++ serv.py
@@ -9,9 +9,9 @@ class Serv(BaseHTTPRequestHandler):
         try:
             file_to_open = open(self.path[1:]).read()
             self.send_response(200)
-        except:
-            file_to_open = "File not found"
-            self.send_response(404)
+        //except:
+        //    file_to_open = "File not found"
+        //    self.send_response(404)
         self.end_headers()
         self.wfile.write(bytes(file_to_open, 'utf-8'))
 
