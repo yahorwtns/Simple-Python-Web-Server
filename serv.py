ff8e813b3b156d8557917d7bf927798ea9e3270f
Yahor <k.minchenok@gmail.com>
2019-08-19 16:54:35 +0300

    Update serv.py
    comm

--- serv.py
+++ serv.py
@@ -7,11 +7,11 @@ class Serv(BaseHTTPRequestHandler):
         if self.path == '/':
             self.path = '/index.html'
         try:
-            file_to_open = open(self.path).read()
+            file_to_open = open(self.path[1:]).read()
             self.send_response(200)
-        //except:
-        //    file_to_open = "File not found"
-        //    self.send_response(404)
+        #except:
+        #    file_to_open = "File not found"
+        #   self.send_response(404)
         self.end_headers()
         self.wfile.write(bytes(file_to_open, 'utf-8'))
 
